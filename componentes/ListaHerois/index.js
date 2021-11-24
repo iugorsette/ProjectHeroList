5c5148f655754dc1c809dc47fb67fe76ae57c94a
Iugor Sette Pereira <81201021+iugorsette@users.noreply.github.com>
2021-11-23 23:34:54 -0300

    Update index.js
    Inserindo css na modal 
    acrescentando imagem e nome ao selecionar o card.

--- componentes/ListaHerois/index.js
+++ componentes/ListaHerois/index.js
@@ -4,6 +4,21 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import "./estilo.css";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
+
+
 export default function ListaHerois(props) {
     const [open, setOpen] = React.useState(false);
     const [checked, setChecked] = React.useState(false);
@@ -28,7 +43,12 @@ export default function ListaHerois(props) {
                 open={open}
                 onClose={handleClose}
             >
-                <Box sx={{ width: 400 }}>
+                <Box sx={{ ...style, width: 400 }}>
+                    <div className="nomeHeroi">
+                        {props.hero}
+                    </div>
+
+                    <img src={props.imagem} alt={props.heroi} />
                     <div>
                         Com: {props.combat} - Dur: {props.durability} - Int: {props.intelligence} -
                         Pwd: {props.power} - Spd: {props.speed} - Str: {props.strength}
