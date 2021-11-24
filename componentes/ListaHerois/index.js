508e2297eced9c1a1b505aa76044d652157057c9
Iugor Sette Pereira <81201021+iugorsette@users.noreply.github.com>
2021-11-23 23:15:05 -0300

    Update index.js
    Implementacao da modal com erro ao fechar a modal. 
    provavel utilizacao inapropriado do Material UI.
    Sem select para luta.

--- componentes/ListaHerois/index.js
+++ componentes/ListaHerois/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
 import "./estilo.css";
 
 export default function ListaHerois(props) {
+    const [open, setOpen] = React.useState(false);
+    const [checked, setChecked] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const select = checked.toString();
     return (
-        <div className="boxHeroi">
+        <Button onClick={handleOpen} className="boxHeroi">
+
             <div className="nomeHeroi">
                 {props.hero}
             </div>
 
             <img className="imagemHeroi" src={props.imagem} alt={props.heroi} />
 
-            <div className="status">
-                Com: {props.combat} - Dur: {props.durability} - Int: {props.intelligence} -
-                Pwd: {props.power} - Spd: {props.speed} - Str: {props.strength} 
-            </div>
-        </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            >
+                <Box sx={{ width: 400 }}>
+                    <div>
+                        Com: {props.combat} - Dur: {props.durability} - Int: {props.intelligence} -
+                        Pwd: {props.power} - Spd: {props.speed} - Str: {props.strength}
+
+                        <Button onClick={handleClose}> X </Button>
+                    </div>
+                </Box>
+
+
+            </Modal>
+        </Button>
     )
-}
+};
